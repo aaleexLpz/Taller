6d1d7699cf7487e8ca6079609d489f4d084b041d
Alejandro <alejandrolc2003@gmail.com>
2024-05-27 20:09:42 +0200

    cambios front 27/05/2024

--- src/main/resources/templates/consultData.js
+++
@@ -1,8 +0,0 @@
-function consultData() {
-    fetch('consultData.php')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('#tablaClientes tbody').innerHTML = data;
-        })
-        .catch(error => console.error('Error al obtener los datos de la base de datos:', error));
-}
